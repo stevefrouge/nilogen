6eb6cb0071ad46fe5b81dd597bb9f7b83404c598
sjf6598 <sjf6598@gmail.com>
2016-04-30 00:30:34 -0400

    angular

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var test = require('./routes/test');
+var landing = require('./routes/landing');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/views/landing', landing);
+app.use('/views/test',test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
