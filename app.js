14f0371571d1234643e90cfe8fbdda17a3b5bca6
sjf6598 <sjf6598@gmail.com>
2016-05-02 00:59:01 -0400

    app path

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//var appRoot = require('app-root-path');
 
 //var routes = require('./routes/index');
 //var users = require('./routes/users');
 //var test = require('./routes/test');
 //var landing = require('./routes/landing');
-//var routes = require('./routes/routes')
+var routes = require('./routes/routes')
 
 var app = express();
 
@@ -24,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(routes);
+app.use(routes);
 //app.use('/', routes);
 //app.use('/users', users);
 //app.use('/views/landing', landing);
@@ -33,18 +34,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 //   res.render('about', { title: 'Express' });
 //});
 
-var title = "Nilogen"
+/*var title = "Nilogen"
 app.get('/', function(req, res, next) {
   res.render('index', { title: title });
 });
 app.get('/views/about', function(req, res){
    res.render('about', { title: title });
 });
-/* GET home page. */
+
 app.get('/views/test', function(req, res, next) {
   res.render('test', { title: title });
 });
-/* GET home page. */
+
 app.get('/views/landing', function(req, res, next) {
   res.render('landing', { title: title });
 });
@@ -53,7 +54,7 @@ app.get('/views/tech', function(req, res, next) {
 });
 app.get('/views/news', function(req, res, next) {
   res.render('news', { title: title });
-});
+});*/
 
 
 
--- routes/routes.js
+++ routes/routes.js
@@ -2,27 +2,27 @@ var express = require('express');
 var app = express();
 
 var title = "Nilogen"
-
-
+var appRoot = require('app-root-path');
+var path = appRoot + "/views/";
 app.get('/', function(req, res, next) {
-  res.render('index', { title: title });
+  res.render(path + 'index', { title: title });
 });
 app.get('/views/about', function(req, res){
-   res.render('about', { title: title });
+   res.render(path + 'about', { title: title });
 });
 /* GET home page. */
 app.get('/views/test', function(req, res, next) {
-  res.render('test', { title: title });
+  res.render(path + 'test', { title: title });
 });
 /* GET home page. */
 app.get('/views/landing', function(req, res, next) {
-  res.render('landing', { title: title });
+  res.render(path + 'landing', { title: title });
 });
 app.get('/views/tech', function(req, res, next) {
-  res.render('tech', { title: title });
+  res.render(path + 'tech', { title: title });
 });
 app.get('/views/news', function(req, res, next) {
-  res.render('news', { title: title });
+  res.render(path + 'news', { title: title });
 });
 
 
