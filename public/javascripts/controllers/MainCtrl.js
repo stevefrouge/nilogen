43526a08552ecb19ef350c351b8334a6aff01e5b
sjf6598 <sjf6598@gmail.com>
2016-05-02 22:08:35 -0400

    mobile menu

--- public/javascripts/controllers/MainCtrl.js
+++ public/javascripts/controllers/MainCtrl.js
@@ -2,5 +2,25 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
     $scope.tagline = 'To the moon and back!';   
+    $scope.menuopen = false;
 
+    $scope.doMenu = function(){
+        if($scope.menuopen){
+              $(".menumobilerow").slideUp( "slow", function() {
+                 $scope.menuopen = false;
+         });
+        }else{
+              $(".menumobilerow").slideDown( "slow", function() {
+                $scope.menuopen = true;
+         });
+        }
+     
+    }
+    $scope.menuClicked = function(){
+       window.scrollTo(0, 0);
+       $(".menumobilerow").slideUp( "slow", function() {
+                 $scope.menuopen = false;
+                
+       });
+    }
 });
