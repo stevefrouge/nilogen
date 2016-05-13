bee8d8d929c023396d60365a2355dd0436bf81a1
sjf6598 <sjf6598@gmail.com>
2016-05-13 17:12:19 -0400

    fix js 2

--- routes/contact.js
+++ routes/contact.js
@@ -10,12 +10,12 @@ Contact.prototype = {
     recievecontactform: function (req, res) {
         var self = this;
         var item = req.body;            
-        self.contact(item, function (loggedin) {
+        /*self.contact(item, function (loggedin) {
             if (loggedin) {
                res.redirect('/loggedin');
             }else{
                 res.redirect('/');
             }            
-        });              
+        });    */          
     }     
 };
