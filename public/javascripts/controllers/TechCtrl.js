7d1205ac89de09e0eb921b6456fad7faed7ca3bc
sjf6598 <sjf6598@gmail.com>
2016-06-10 11:35:32 -0400

    fixes tech

--- public/javascripts/controllers/TechCtrl.js
+++ public/javascripts/controllers/TechCtrl.js
@@ -1,11 +1,12 @@
 angular.module('TechCtrl', []).controller('TechController', function($scope) {
 
      $scope.img = '';
-     $scope.imgClicked = function(obj){
+     $scope.imgClicked = function(obj,text){
         
          //alert(obj);
          $scope.img = obj;
          $("#maxtechimg").attr("src", obj);
          $('#myModal').modal('toggle');
+         $('#myModalLabel').html(text);
      }
 });
